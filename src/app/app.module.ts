8b90c6ada2a3b99ce3d1ceb813da3c593577b25f
KeithRitter <ritter.keithpersonal@gmail.com>
2020-05-15 19:40:24 -0400

    Updated navbar and sidebar design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
