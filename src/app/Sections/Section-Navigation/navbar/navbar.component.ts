55188a8e1299665b6ee7b111ef7f57853d4e944a
KeithRitter <ritter.keithpersonal@gmail.com>
2020-05-16 00:53:17 -0400

    Added mobile functionality for sidebar and navbar

--- src/app/Sections/Section-Navigation/navbar/navbar.component.ts
+++ src/app/Sections/Section-Navigation/navbar/navbar.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  styles: ['@media (max-width: 1180px) { .mobileNav { display: inline !important;} }'
+]
 })
 export class NavbarComponent implements OnInit {
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: ['@media (max-width: 1180px) { .sidebar { display: none !important;} }']
 })
 export class AppComponent {
   title = 'angularjs-app';
